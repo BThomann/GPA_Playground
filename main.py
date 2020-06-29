0baaf49a69130aa10bac13065c9178be364164e1
BenJey <thomann.benni@gmail.com>
2020-06-29 09:13:15 +0200

    files added

--- main.py
+++ main.py
@@ -1,1 +1,10 @@
 # This is the main of the playground
+def my_print(a, b):
+    if a <= b:
+        m = (a + b) // 2
+        my_print(a, m - 1)
+        print(m, end="")
+        my_print(m + 1, b)
+
+
+my_print(1, 100)
